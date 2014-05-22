753c9f243150b4e3de2f1683e9204cb27e4f84df
Aaron Forgue <forgue@gmail.com>
2014-05-21 22:59:28 -0400

    Update config_example to include recent changes

--- config_example.php
+++ config_example.php
@@ -10,7 +10,21 @@ define( 'MMAC_WP_CORE_PATH', '/path/to/wordpress/' );
 // Relative or absolute path to easy-digital-downloads plugin directory
 define( 'MMAC_EDD_PATH', '/path/to/wp-content/plugins/easy-digital-downloads/' );
 
-define( 'MMAC_EMAIL_NOTIFICATION_SUBJECT', 'xxxxx' );
+// Email Notification Subject and Content
+
+// Use this setting to route all email notifications to the specified email address
+// Comment this setting out to send email notificaitons to real users
+//define( 'MMAC_EMAIL_NOTIFICATION_TEST_EMAIL', 'example@example.com' );
+
+define( 'MMAC_EMAIL_NOTIFICATION_SUBJECT_NEW_USER', 'xxxxxx' );
+define( 'MMAC_EMAIL_NOTIFICATION_CONTENT_NEW_USER', "
+	yyyyyy {EMAIL} yyyyyy {PASSWORD} yyyyyy
+" );
+
+define( 'MMAC_EMAIL_NOTIFICATION_SUBJECT_EXISTING_USER', 'xxxxxx' );
+define( 'MMAC_EMAIL_NOTIFICATION_CONTENT_EXISTING_USER', "
+	yyyyyy {EMAIL} yyyyyy
+" );
 
 // Mojo Marketplace / Easy Digital Downloads Product Map
 // MM_Product_ID => EDD_Post_ID
